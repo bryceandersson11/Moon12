c10dfffa2e71e9678a6862df824cddb0107a16e1
ImDamc <damcproseeee@outlook.com>
2025-02-23 17:56:43 -0500

    Update 6872274481.lua

--- Games/6872274481.lua
+++ Games/6872274481.lua
@@ -1,3 +1,9 @@
 if script_key ~= nil then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b9c3e0faae2dbdacadd8f55e9c3382a.lua"))()
+
+    if shared.MoonBeta then
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b9c3e0faae2dbdacadd8f55e9c3382a.lua"))()
+    else
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/577e2c7c4368b22b7d8b66daaea008ed.lua"))()
+    end
+    
 end
