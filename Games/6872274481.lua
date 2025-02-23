5254d077c16080f6863564558da8be722516ca53
ImDamc <damcproseeee@outlook.com>
2025-02-23 17:57:30 -0500

    Update 6872274481.lua

--- Games/6872274481.lua
+++ Games/6872274481.lua
@@ -1,4 +1,4 @@
-if script_key ~= nil then
+if getgenv().script_key ~= nil then
 
     if shared.MoonBeta then
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b9c3e0faae2dbdacadd8f55e9c3382a.lua"))()
