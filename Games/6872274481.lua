e489c30d2b0281f0950ed35ef6474f96045aff3a
ImDamc <damcproseeee@outlook.com>
2025-05-12 20:27:19 -0400

    Delete Games/6872274481.lua

--- Games/6872274481.lua
+++
@@ -1,9 +0,0 @@
-if getgenv().script_key ~= nil then
-
-    if shared.MoonBeta then
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b9c3e0faae2dbdacadd8f55e9c3382a.lua"))()
-    else
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/577e2c7c4368b22b7d8b66daaea008ed.lua"))()
-    end
-    
-end
