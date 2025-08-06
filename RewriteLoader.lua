0a0e228d4a12fc3fb47bc7b80e17238e2d5025e0
ImDamc <damcproseeee@outlook.com>
2025-08-06 18:11:50 -0400

    Update RewriteLoader.lua

--- RewriteLoader.lua
+++ RewriteLoader.lua
@@ -1,5 +1,5 @@
+local playersService = game:GetService("Players")
 if getgenv().moonkey == nil then
-  --return error("Please define your key first!")
   getgenv().moonkey = "none"
 end
 
@@ -7,8 +7,8 @@ if getgenv().gethwid == nil then
     getgenv().gethwid = function() return "swaefgserfsegrfgse8rfgsgerisrgdsrethgrdthtrdjtyj" end
 end
 
-if game:GetService("Players").LocalPlayer.Character == nil then
-    repeat task.wait(1) until game:GetService("Players").LocalPlayer.Character ~= nil
+if playersService.LocalPlayer.Character == nil then
+    repeat task.wait(1) until playersService.LocalPlayer.Character ~= nil
 end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/Moon/refs/heads/main/Script/Obfuscated.lua"))()
