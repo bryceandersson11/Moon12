d26c1d51b4262594d40a30f10297b3271bfeb529
Ezezez <105952092+noobieispro@users.noreply.github.com>
2025-08-16 10:58:36 -0400

    Update RewriteLoader.lua

--- RewriteLoader.lua
+++ RewriteLoader.lua
@@ -11,4 +11,4 @@ if playersService.LocalPlayer.Character == nil then
     repeat task.wait(1) until playersService.LocalPlayer.Character ~= nil
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/Moon/refs/heads/main/Script/Obfuscated.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noobieispro/Moon12/refs/heads/main/Script/Obfuscated.lua"))()
